6e1681ec6e2464e60017fcada04abbb95b5858ee
wrkbr <zcarc1@gmail.com>
2020-03-25 14:54:17 +0900

    #1.0 Layouts with Flexbox in React Native

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello!</Text>
+      <View style={styles.yellowView} />
+      <View style={styles.blueView} />
     </View>
   );
 }
@@ -12,12 +13,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 
-  text: {
-    color: 'blue',
+  yellowView: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor: 'blue'
   }
 });

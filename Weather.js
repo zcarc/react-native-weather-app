bcc4ae74c7cc4f264b28a6036ed0b78b6cdfc40a
wrkbr <zcarc1@gmail.com>
2020-03-26 14:53:40 +0900

    #2.4 Titles and Subtitles

--- Weather.js
+++ Weather.js
@@ -4,64 +4,92 @@ import PropTypes from 'prop-types';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 
+
 const weatherOptions = {
-  Haze: {
-    iconName: "weather-hail",
-    gradient: ["#4DA0B0", "#D39D38"]
-  },
-
-  Clouds: {
-    iconName: "weather-hail",
-    gradient: ["#4DA0B0", "#D39D38"]
-  },
-
-  Thunderstorm: {
-    iconName: "",
-    gradient: []
-  },
-  Drizzle: {
-    iconName: "",
-    gradient: []
-  },
-  Rain: {
-    iconName: "",
-    gradient: []
-  },
-  Snow: {
-    iconName: "",
-    gradient: []
-  },
-  Atmosphere: {
-    iconName: "",
-    gradient: []
-  },
-  Clear: {
-    iconName: "",
-    gradient: []
-  }
+    Thunderstorm: {
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#4286f4"]
+      },
+      Drizzle: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
+      },
+      Rain: {
+        iconName: "weather-rainy",
+        gradient: ["#00C6FB", "#005BEA"]
+      },
+      Snow: {
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"]
+      },
+      Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
+      },
+      Clear: {
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"]
+      },
+      Clouds: {
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"],
+        title: "Clouds",
+        subtitle: "You can go outside."
+      },
+      Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+      },
+      Dust: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+      },
+      Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "Haze",
+        subtitle: "Just don't go outside."
+      }
 };
 
 export default function Weather({temp, condition}) {
 
-    return (
+    console.log(condition);
 
+    return (
       <LinearGradient
-        colors={weatherOptions[condition].gradient}
+        colors={weatherOptions["Thunderstorm"].gradient}
         style={styles.container}
       >
+        <StatusBar barStyle="light-content" />
 
-        <StatusBar barStyle="light-content"/>
         <View style={styles.halfContainer}>
           <MaterialCommunityIcons
             size={96}
             name={weatherOptions[condition].iconName}
-            color='white'
+            color="white"
           ></MaterialCommunityIcons>
           <Text style={styles.temp}>{temp}</Text>
         </View>
 
-        <View style={styles.halfContainer} />
+        <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+          <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+          <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+        </View>
 
+        {/* <View style={styles.halfContainer}>
+          <Text style={styles.temp}>{temp}</Text>
+        </View> */}
+
+        {/* <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+          <Text style={styles.temp}>{temp}</Text>
+        </View> */}
+
+        {/* <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+          <Text style={styles.title}>In case this is too long.</Text>
+          <Text style={styles.subtitle}>and this one as well.</Text>
+        </View> */}
+        
       </LinearGradient>
     );
 }
@@ -81,18 +109,36 @@ Weather.propTypes = {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    temp: {
-        fontSize: 42,
-        color: 'white',
-    },
-    halfContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  temp: {
+    fontSize: 42,
+    color: "white"
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10,
+  },
+  subtitle: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "600",
+  },
+  textContainer: {
+      paddingHorizontal: 20,
+      alignItems: 'flex-start',
+  }
+});
+
+
+// https://uigradients.com/
